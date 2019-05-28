ee8fcfd9de337a08c4897d006d7d1db72a121af9
anderstofte <ande587e@stud.kea.dk>
2019-05-28 13:39:44 +0200

    reset password api method works

--- api/endpoints/user.js
+++ api/endpoints/user.js
@@ -1,22 +1,31 @@
 let app
 let mysqlModels
+let mysqlDb
 
-module.exports.initialize = function (newApp, newMysqlModels) {
+module.exports.initialize = function (newApp, newMysqlModels, newMysqlDb) {
     app = newApp
     mysqlModels = newMysqlModels
+    mysqlDb = newMysqlDb
 
     getUser()
     deleteUser()
     updateUser()
-    postUser()
+    createUser()
     getAllUsers()
-
+    resetUserPassword()
 }
 
 
 function getUser() {
-    
-
+    app.get('/api/users/:userId', (req, res) => {
+        mysqlModels.user.findOne({ where: { id: req.params.userId }})
+        .then(queryRes => {
+            res.send(queryRes)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+    })
 }
 
 function deleteUser() {
@@ -24,17 +33,60 @@ function deleteUser() {
 }
 
 function updateUser() {
-
+    app.post('/api/users/:userId', (req, res) => {
+        let user;
+        
+        mysqlModels.user.update(req.body, { where: { id: req.params.userId }} )
+        .then(queryRes => {
+            res.send(queryRes)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+    })
 }
 
-function postUser() {
+function createUser() {
     app.post('/api/users/create', (req, res) => {
-        console.log(req.body)
-        // TODO: opret en record baseret på req.body
+        mysqlModels.user.create({
+            "email": req.body.email,
+            "password": req.body.password,
+            "address": req.body.address,
+            "first_name": req.body.first_name,
+            "last_name": req.body.last_name,
+            "will_delete_at": null
+        })
+        .then(() => {
+            res.send('user created')
+        })
+        .catch(error => {
+            res.send(error)
+        })
     })
 }
 
 function getAllUsers() {
-    
+    app.get('/api/users', (req, res) => {
+        mysqlModels.user.findAll()
+        .then(queryRes => {
+            res.send(queryRes)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+    })
+}
+
+function resetUserPassword() {
+    app.get('/api/users/:userId/reset-password', (req, res) => {
+        mysqlDb.db.query('CALL reset_password (:user_id)',
+        { replacements: { user_id: req.params.userId}})
+        .then(() => {
+            res.send('The password has been reset successfully.')
+        })
+        .catch(error => {
+            res.send(error)
+        })
+    })
 }
 
