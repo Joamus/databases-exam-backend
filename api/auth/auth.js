8e4352eb8484b85a31e3e7f89dcb05d65d3fa079
Joakim Ousager Andersen <joakimandersen@Joakims-MacBook-Pro-2.local>
2019-05-28 15:39:21 +0200

    Auth

---
+++ api/auth/auth.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const fs = require('fs')
+const jwt = require('jsonwebtoken')
+
+function generateToken(payload) {
+
+    let privateKey = fs.readFileSync('./api/auth/private.key', 'utf8')
+
+    let signOptions = {
+        issuer: 'gjk portal',
+        audience: 'http://gjk.dk',
+        algorithm: "RS256"
+    }
+
+    let token = jwt.sign(payload, privateKey, signOptions)
+    return token;
+
+}
+
+function verifyToken(token) {
+
+    let publicKey = fs.readFileSync('./api/auth/public.key', 'utf8')
+    
+
+    let verifyOptions = {
+        issuer: 'gjk portal',
+        audience: 'http://gjk.dk',
+        algorithm: ["RS256"]
+    }
+
+    let legit = jwt.verify(token, publicKey, verifyOptions)
+    return legit;
+
+}
+
+function authenticateRequest(req, res, next) {
+    if (req.path == '/api/login') {
+      return next();
+    }
+    let decoded
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      decoded = verifyToken(token)
+      res.locals.user = decoded;
+      next()
+    } catch(err) {
+      if (!decoded) {
+        res.status(401).json({message: 'Please provide valid token', status: 401})
+      }
+    }
+}
+
+module.exports = {
+  verifyToken,
+  generateToken,
+  authenticateRequest
+}
